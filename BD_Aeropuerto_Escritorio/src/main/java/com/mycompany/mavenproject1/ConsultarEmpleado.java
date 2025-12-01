a14b6c96a47ab739ec329b2acda10d85326fb2f1
Jesus Govanny Valdez C <jesus.vc@tecjerez.edu.mx>
2025-11-30 23:51:16 -0600

    Se corrigio funcionalidad de buscar en fase de pruebas

--- BD_Aeropuerto_Escritorio/src/main/java/com/mycompany/mavenproject1/ConsultarEmpleado.java
+++ BD_Aeropuerto_Escritorio/src/main/java/com/mycompany/mavenproject1/ConsultarEmpleado.java
@@ -344,13 +344,16 @@ public class ConsultarEmpleado extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-          MenuInicio menu = new MenuInicio();   // crear ventana principal
+          MenuEmpleados menu = new MenuEmpleados();   // crear ventana principal
        
     menu.setVisible(true);                // mostrar
 
     this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    
+    
+    
     private void filtrarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_filtrarBtnActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_filtrarBtnActionPerformed
--- BD_Aeropuerto_Escritorio/src/main/java/com/mycompany/mavenproject1/EliminarEmpleado.java
+++ BD_Aeropuerto_Escritorio/src/main/java/com/mycompany/mavenproject1/EliminarEmpleado.java
@@ -110,6 +110,11 @@ public class EliminarEmpleado extends javax.swing.JFrame {
         jButton3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jButton3.setForeground(new java.awt.Color(255, 255, 255));
         jButton3.setText("Eliminar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -184,6 +189,15 @@ public class EliminarEmpleado extends javax.swing.JFrame {
 
     this.dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    
+    
+    
+    
+    
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
