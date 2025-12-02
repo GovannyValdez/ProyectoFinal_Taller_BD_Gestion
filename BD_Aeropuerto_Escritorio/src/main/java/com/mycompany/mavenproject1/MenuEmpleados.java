e36972aad0c87b8a7c932ab564aa9aff529348a6
Jesus Govanny Valdez C <jesus.vc@tecjerez.edu.mx>
2025-12-01 23:29:58 -0600

    Se arreglo algunas funcionalidades del uso de las ventanas

--- BD_Aeropuerto_Escritorio/src/main/java/com/mycompany/mavenproject1/MenuEmpleados.java
+++ BD_Aeropuerto_Escritorio/src/main/java/com/mycompany/mavenproject1/MenuEmpleados.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.mycompany.mavenproject1;
-
+import java.awt.Window; 
 /**
  *
  * @author govan
@@ -17,8 +17,24 @@ public class MenuEmpleados extends javax.swing.JFrame {
      */
     public MenuEmpleados() {
         initComponents();
+    setLocationRelativeTo(null);
+    cerrarTodasLasVentanasExceptoActual(); 
     }
 
+    public void cerrarTodasLasVentanasExceptoActual() {
+    // Obtener todas las ventanas abiertas
+    Window[] windows = Window.getWindows();
+    
+    for (Window window : windows) {
+        // Cerrar todas las ventanas excepto esta misma
+        if (window != this && window.isVisible()) {
+            window.dispose();
+        }
+    }
+}
+    
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
